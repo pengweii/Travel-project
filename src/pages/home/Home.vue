43651af79ef350e2b436dad12349086043594e60
pengwei <mr.pengwei@outlook.com>
2021-02-20 23:21:30 +0800

    add header

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,10 +1,17 @@
 <template>
-  <div>Hello World</div>
+  <div>
+    <home-header></home-header>
+  </div>
 </template>
 
 <script>
+
+import HomeHeader from './components/Header'
 export default {
-  name: 'Home'
+  name: 'Home',
+  components: {
+    HomeHeader: HomeHeader
+  }
 }
 </script>
 
