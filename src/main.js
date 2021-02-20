ee268f6434144f1baa514dc77829ccac9522d912
pengwei <mr.pengwei@outlook.com>
2021-02-20 19:40:42 +0800

    project initialized

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import fastClick from 'fastclick'
+import './assets/styles/reset.css'
+// 解决某些机型多倍屏的问题
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+// 解决某些机型click事件有300毫秒延时执行的问题
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
