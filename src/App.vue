e8f07c160a2c96bb5c570f736e61da2c95e203e4
Peng Wei <mr.pengwei@outlook.com>
2021-02-25 14:10:16 +0800

    finish pages

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,10 @@
   <div id="app">
     <!-- 路由就是根据网址不同返回不同的内容 -->
     <!-- 显示的是当前路由地址所对应的内容 -->
-    <router-view />
+    <!-- keep-alive: 路由中的内容加载时放在内存中，避免每次加载 -->
+    <keep-alive>
+      <router-view />
+    </keep-alive>
   </div>
 </template>
 
